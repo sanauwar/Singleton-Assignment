9ac31e72c6921a49d35f8a9756edee32011638ac
sanauwar <sanauwarnadeem123@gmail.com>
2023-08-04 05:39:21 +0530

    Registration completed

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const { app, port } = require('./config/express')
 
 
-// app.post('/test', (req, res) => {
-//     const body = req.body
-//     res.send({ data: body, message: "Post is working" });
-// })
-
 app.listen(port, () => {
     console.log('port is listning', port);
 })
