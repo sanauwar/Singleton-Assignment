42b689be602aace11bf583747f57a05814b969dd
sanauwar <sanauwarnadeem123@gmail.com>
2023-08-06 05:22:51 +0530

    Candidate module Route

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const test = require('./test')
 const users = require('./users')
 const login = require('./login')
+const candidates = require('./candidates')
 //For authorization
 const dmz = require('./dmz')
 
@@ -11,6 +12,7 @@ router.all('*', dmz.enforce);
 router.use('/test', test)
 router.use('/user', users)
 router.use('/login', login)
+router.use('/candidates', candidates)
 
 module.exports = router
 
